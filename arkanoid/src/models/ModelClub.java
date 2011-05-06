45ce7f7a01256f6155bbd60c691f9d501f68209c
martins.ajc <martins.ajc@gmail.com>
2011-05-06 17:48:14 +0000

    Modelo de entidades inicial.

---
+++ arkanoid/src/models/ModelClub.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arkanoid.models;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author sPeC
+ */
+public class ModelClub  extends Observable
+{
+    
+}
