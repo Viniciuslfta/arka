10de43fbaf317fb236242c86f6a80378f694566c
martins.ajc@gmail.com <martins.ajc@gmail.com@5750694a-7dd9-3e99-4c15-4fa721874da3>
2011-06-15 15:59:13 +0000

    Evento de mudança de vista (geom/textura)

---
+++ trunk/arkanoid/src/arkanoid/replay/ChangeViewEvent.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arkanoid.replay;
+
+import arkanoid.RegisteredPlayerData;
+import arkanoid.models.ModelPlayArea;
+import org.lwjgl.opengl.GL11;
+
+/**
+ *
+ * @author sPeC
+ */
+public class ChangeViewEvent extends GameEvent{
+
+    boolean mUseTex;
+    public ChangeViewEvent(boolean _useTex, long _timeStamp) {
+        super(_timeStamp);
+        mUseTex = _useTex;
+    }
+
+    
+    @Override
+    public void execute(ModelPlayArea _model) {
+
+            if (!mUseTex) {
+                GL11.glDisable(GL11.GL_DEPTH_TEST);
+                GL11.glDisable(GL11.GL_LIGHTING);
+                GL11.glDisable(GL11.GL_LIGHT0);
+                RegisteredPlayerData.getInstance().drawTextures(false);
+            } else {
+                // Passa a desenhar texturas
+                GL11.glEnable(GL11.GL_LIGHTING);
+                GL11.glEnable(GL11.GL_LIGHT0);
+                RegisteredPlayerData.getInstance().drawTextures(true);
+            }
+    }
+    
+}
